003bd2a3686e1b6f7f0c373687d5347fd973b913
asabljak <asabljak@tvz.hr>
2018-01-15 17:16:52 +0100

    manifest details

---
+++ RMA/RMA/Model/Data.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RMA.Model
+{
+    class Data
+    {
+        public bool isOn { get; set; }
+        public bool backlight { get; set; }
+        public int wanTemp { get; set; }
+        public double curTemp { get; set; }
+    }
+}
